3a237428c791fbc3e5a6ac805ad83d10b023889d
Omar <eng16.omarmohamed@gmail.com>
2025-10-06 00:39:48 +0300

    updated main file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import data_router
+from routes import data_router, base_router
 
-app = FastAPI(
-    title="Water Segmentation API",
-    description="API for detecting and segmenting water bodies in images",
-)
+app = FastAPI()
+app.include_router(base_router)
+app.include_router(data_router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,9 +14,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(data_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Water Segmentation API is running"}
 
